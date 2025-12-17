d92c5f4d8085a19c52015d42ed3fa79734f00648
Temi <limiajayi@gmail.com>
2025-12-17 12:11:35 +0000

    Added a pause state and a pause button to be able to pause quotes at will #2

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -3,26 +3,37 @@ import { useState, useEffect } from "react";
 const Selection = ({ lines }) => {
 
     //stores a random number based on the current length of quote
-    const [selected, setSelected] = useState(Math.floor(Math.random() * lines.length))
+    const [selected, setSelected] = useState(0)
+    const [pause, setPause] = useState(false)
 
-    //change quotes every 10 seconds
+    //change quotes every 15 seconds
+
     useEffect(() => {
+        //if paused don't start the interval
+        if (pause) return;
+
         const interval = setInterval(() => {
             
             const newSelected = Math.floor(Math.random() * lines.length)
             setSelected(newSelected)
 
-        }, 20000);
+        }, 15000);
 
         //clears the effect
         return () => clearInterval(interval)
-    }, [lines]);
+    }, [lines, pause]);
 
 
 
     return (
         <>
             <p>{lines[selected]}</p>
+
+            <button onClick={() => {setPause(!pause)}}>
+
+                {pause ? 'Resume' : 'Pause'}
+                
+            </button>
         </>
     )
 
